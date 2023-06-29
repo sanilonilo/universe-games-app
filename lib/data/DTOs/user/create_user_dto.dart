646c6089b3b600fd22cf1bc37175974d45ead039
Sanilo Alves <saniloalves0@gmail.com>
2023-06-29 16:30:39 -0300

    refactor: edit user dto

--- lib/data/DTOs/user/create_user_dto.dart
+++ lib/data/DTOs/user/create_user_dto.dart
@@ -1,6 +1,13 @@
-abstract class CreateUserDTO{
-  late String name;
-  late String email;
-  late String password;
-  late String confirmPassword;
+class CreateUserDTO{
+  String name;
+  String email;
+  String password;
+  String confirmPassword;
+
+  CreateUserDTO({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.confirmPassword
+  });
 }
