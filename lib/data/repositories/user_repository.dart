d525f05e6154ac3912d39c6e87a5e47579b590f2
Sanilo Alves <saniloalves0@gmail.com>
2023-06-29 19:58:30 -0300

    feat: add auth user repository

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,5 +1,11 @@
 import '../DTOs/user/create_user_dto.dart';
+import '../DTOs/user/auth_user_dto.dart';
+import '../../domain/entities/user.dart';
 
 abstract class CreateUserRepository{
   Future<void> create(CreateUserDTO dto);
+}
+
+abstract class AuthUserRepository{
+  Future<User> auth(AuthUserDTO dto);
 }
