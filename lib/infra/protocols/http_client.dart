38c33012b5029b6c0faf02a62370d1e14ab9e11a
Sanilo Alves <saniloalves0@gmail.com>
2023-06-29 21:23:55 -0300

    refactor: change http client protocol

--- lib/infra/protocols/http_client.dart
+++ lib/infra/protocols/http_client.dart
@@ -1,6 +1,6 @@
-abstract class HttpClient {
-  Future<dynamic> get(String endpoint,dynamic body, dynamic config);
-  Future<dynamic> post(String endpoint,dynamic body, dynamic config);
-  Future<dynamic> put(String endpoint,dynamic body, dynamic config);
-  Future<dynamic> delete(String endpoint,dynamic body, dynamic config);
+abstract class HttpClient <Response>{
+  Future<Response> get(String endpoint,dynamic body, dynamic config);
+  Future<Response> post(String endpoint,dynamic body, dynamic config);
+  Future<Response> put(String endpoint,dynamic body, dynamic config);
+  Future<Response> delete(String endpoint,dynamic body, dynamic config);
 }
