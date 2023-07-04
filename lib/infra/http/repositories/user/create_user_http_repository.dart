77fdfc82f225173ad3cc41f79c8a08e15b738d56
Sanilo Alves <saniloalves0@gmail.com>
2023-07-04 20:40:01 -0300

    refactor: move folder

--- lib/infra/http/repositories/user/create_user_http_repository.dart
+++ lib/infra/http/repositories/user/create_user_http_repository.dart
@@ -1,6 +1,6 @@
 import 'package:app/data/DTOs/user/create_user_dto.dart';
 import 'package:app/data/repositories/user_repository.dart';
-import 'package:app/infra/helpers/http_response.dart';
+import 'package:app/infra/http/helpers/http_response.dart';
 import 'package:app/infra/protocols/http_client.dart';
 
 class CreateUserHttpRepository implements CreateUserRepository{
