5f394f7039dd5250af6bf52094cd38b93ffbc7bf
Sanilo Alves <saniloalves0@gmail.com>
2023-07-04 19:48:36 -0300

    test: complete test create user http repository

--- test/infra/http/repositories/http_client_stub.dart
+++ test/infra/http/repositories/http_client_stub.dart
@@ -4,27 +4,28 @@ import 'package:app/infra/helpers/http_response.dart';
 import 'package:app/infra/protocols/http_client.dart';
 
 class HttpClientStub implements HttpClient<HttpResponse>{
+
+  int statusMock;
+
   @override
   Future<HttpResponse> get(String endpoint, body, config) {
-    // TODO: implement get
-    throw UnimplementedError();
+    return Future(() => HttpResponse(status: statusMock));
   }
 
   @override
   Future<HttpResponse> post(String endpoint, body, config) {
-    // TODO: implement post
-    return Future(() => HttpResponse(status: 200));
+    return Future(() => HttpResponse(status: statusMock));
   }
 
   @override
   Future<HttpResponse> put(String endpoint, body, config) {
-    // TODO: implement put
-    throw UnimplementedError();
+    return Future(() => HttpResponse(status: statusMock));
   }
 
   @override
   Future<HttpResponse> delete(String endpoint, body, config) {
-    // TODO: implement delete
-    throw UnimplementedError();
+    return Future(() => HttpResponse(status: statusMock));
   }
+
+  HttpClientStub({required this.statusMock});
 }
--- test/infra/http/repositories/user/create_user_http_repository_test.dart
+++ test/infra/http/repositories/user/create_user_http_repository_test.dart
@@ -1,7 +1,4 @@
 import 'package:app/data/DTOs/user/create_user_dto.dart';
-import 'package:app/infra/protocols/http_client.dart';
-import 'package:mockito/mockito.dart';
-import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
 import 'package:app/infra/http/repositories/user/create_user_http_repository.dart';
 
@@ -11,13 +8,22 @@ String endpoint = 'signup';
 
 void main(){
   group('Create user http repository', () {
-    test('Create user http repository: throw error return status 400', () async {
-      HttpClientStub httpClient = HttpClientStub();
+    test('Create user http repository: return status 400', () async {
+      HttpClientStub httpClient = HttpClientStub(statusMock:400);
       CreateUserHttpRepository sut = CreateUserHttpRepository(httpClient: httpClient,endpoint: endpoint);
       CreateUserDTO data = CreateUserDTO(name: 'name_test',email: 'email@test',password: 'password_test',confirmPassword: 'confirm_password_test');
       var response = await sut.create(data);
 
       expect(response.status,400);
+    });
+
+    test('Create user http repository: return status 200', () async {
+      HttpClientStub httpClient = HttpClientStub(statusMock:200);
+      CreateUserHttpRepository sut = CreateUserHttpRepository(httpClient: httpClient,endpoint: endpoint);
+      CreateUserDTO data = CreateUserDTO(name: 'name_test',email: 'email@test',password: 'password_test',confirmPassword: 'confirm_password_test');
+      var response = await sut.create(data);
+
+      expect(response.status,200);
     });
   });
 }
