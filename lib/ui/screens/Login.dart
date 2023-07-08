8c55d9b03f4e767040874b9abb4548251d455ef2
Sanilo Alves <saniloalves0@gmail.com>
2023-07-07 21:27:52 -0300

    feat: add new screen login

---
+++ lib/ui/screens/Login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image.asset('assets/images/logo-login.png',width: 120),
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
