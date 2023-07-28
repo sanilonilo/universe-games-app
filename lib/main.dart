82270e13e4fcdcba205b3ddcce71bdbc8143196d
Sanilo Alves <saniloalves0@gmail.com>
2023-07-27 21:23:10 -0300

    style: define theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,46 @@ import 'package:flutter/material.dart';
 import './ui/screens/Login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final ThemeData theme = ThemeData();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.green,
+          secondary: Colors.white,
+        ),
+        textTheme: theme.textTheme.copyWith(
+          titleLarge: const TextStyle(fontSize: 30),
+          titleMedium: const TextStyle(fontSize: 25),
+          titleSmall: const TextStyle(fontSize: 20),
+          bodyLarge: const TextStyle(fontSize: 16),
+          bodyMedium: const TextStyle(fontSize: 14),
+          bodySmall: const TextStyle(fontSize: 12),
+          labelLarge: const TextStyle(fontSize: 11),
+          labelMedium: const TextStyle(fontSize: 10),
+          labelSmall: const TextStyle(fontSize: 9),
+        ),
+        scaffoldBackgroundColor: Colors.black26,
+        inputDecorationTheme: theme.inputDecorationTheme.copyWith(
+          border: const UnderlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white)
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white)
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white)
+          ),
+          labelStyle: TextStyle(color: Colors.white)
+        )
       ),
       home: Login(),
       debugShowCheckedModeBanner: false,
